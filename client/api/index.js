683b1c98df744c34311bfd46bb70048775974ebc
kyogesh42-collab <kyogeshyogesh42@gmail.com>
2026-06-29 11:48:25 +0530

    Add diagnostic error handling wrappers to serverless entry points

--- client/api/index.js
+++ client/api/index.js
@@ -1,3 +1,21 @@
-const app = require('../../server/src/app');
+let app;
+let initError = null;
 
-module.exports = app;
+try {
+  app = require('../../server/src/app');
+} catch (err) {
+  initError = err;
+  console.error('[Vercel Client API Init Error]:', err);
+}
+
+module.exports = (req, res) => {
+  if (initError) {
+    return res.status(500).json({
+      success: false,
+      message: 'Serverless initialization error',
+      error: initError.message,
+      stack: initError.stack,
+    });
+  }
+  return app(req, res);
+};
